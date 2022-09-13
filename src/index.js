76a002b7e92afa864fee4ae8f7a80c7aed97ddb6
tjanelidze <tedojanelidze7@gmail.com>
2022-09-13 17:20:36 +0400

    moved everything in src file created delete function

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 const formEl = document.querySelector('.needs-validation');
 const tbodyEl = document.querySelector('tbody');
+const tableEl = document.querySelector('table');
+const gender = document.querySelector('#gender').value;
+
+const firstName = document.getElementById('firstname').value;
+const lastName = document.getElementById('lastname').value;
+const address = document.getElementById('address').value;
+const dateBirth = document.getElementById('dateOfBirth').value;
+const note = document.getElementById('notes').value;
 
 const validation = function (event) {
   if (formEl.checkValidity() === false) {
@@ -13,11 +21,6 @@ const validation = function (event) {
 const onAddWebsite = function (event) {
   event.preventDefault();
 
-  const firstName = document.getElementById('firstname').value;
-  const lastName = document.getElementById('lastname').value;
-  const address = document.getElementById('address').value;
-  const gender = document.getElementById('gender').value;
-  const dateBirth = document.getElementById('dateOfBirth').value;
   tbodyEl.innerHTML += `
   <tr>
     <th scope="row">1</th>
@@ -27,11 +30,21 @@ const onAddWebsite = function (event) {
     <td>${gender}</td>
     <td>${dateBirth}</td>
     <td class="text-center">
-        <button type="button" class="btn btn-danger">Delete</button>
+        <button type="button" class="deleteBtn btn btn-danger">Delete</button>
     </td>
   </tr>
   `;
 };
 
+const onDeleteRow = (event) => {
+  if (!event.target.classList.contains('deleteBtn')) {
+    return;
+  }
+
+  const btn = event.target;
+  btn.closest('tr').remove();
+};
+
 formEl.addEventListener('submit', validation);
 formEl.addEventListener('submit', onAddWebsite);
+tableEl.addEventListener('click', onDeleteRow);
