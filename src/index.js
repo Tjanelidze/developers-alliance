2a05e98fde59dca4dd4339c03c8caefb4b05bd6c
tjanelidze <tedojanelidze7@gmail.com>
2022-09-14 17:36:35 +0400

    added logic for adding users in table with unique identifier and also deleting them from

--- src/index.js
+++ src/index.js
@@ -1,39 +1,97 @@
 const formEl = document.querySelector('.needs-validation');
 const tbodyEl = document.querySelector('tbody');
 const tableEl = document.querySelector('table');
-const gender = document.querySelector('#gender').value;
+const button = document.getElementById('submitBtn');
+const gender = document.querySelector('#gender');
 
-const firstName = document.getElementById('firstname').value;
-const lastName = document.getElementById('lastname').value;
-const address = document.getElementById('address').value;
-const dateBirth = document.getElementById('dateOfBirth').value;
-const note = document.getElementById('notes').value;
+const firstname = document.getElementById('firstname');
+const lastname = document.getElementById('lastname');
+const address = document.getElementById('address');
+const dateBirth = document.getElementById('dateOfBirth');
+const note = document.getElementById('notes');
+
+let users = [];
+
+// let formall = Array.from(document.querySelectorAll('#main-form input')).reduce(
+//   (acc, input) => ({ ...acc, [input.id]: input }),
+//   {}
+// );
 
 const validation = function (event) {
   if (formEl.checkValidity() === false) {
     event.preventDefault();
     event.stopPropagation();
+    console.log('false');
   }
 
   formEl.classList.add('was-validated');
 };
 
 const onAddWebsite = function (event) {
+  validation(event);
+  if (!formEl.checkValidity()) return;
   event.preventDefault();
 
-  tbodyEl.innerHTML += `
-  <tr>
-    <th scope="row">1</th>
-    <td>${firstName}</td>
-    <td>${lastName}</td>
-    <td>@${address}</td>
-    <td>${gender}</td>
-    <td>${dateBirth}</td>
+  let id = 0;
+
+  if (users.length === 0) {
+    id = 1;
+  } else {
+    id = users[users.length - 1].id + 1;
+  }
+
+  users.push({
+    id: id,
+    firstname: firstname.value,
+    lastname: lastname.value,
+    address: address.value,
+    gender: gender.value,
+    dateBirth: dateBirth.value,
+  });
+
+  tbodyEl.innerHTML = users
+    .map((user) => {
+      return `<tr>
+    <th scope="row">${user.id}</th>
+    <td>${user.firstname}</td>
+    <td>${user.lastname}</td>
+    <td>${user.address}</td>
+    <td>${user.gender}</td>
+    <td>${user.dateBirth}</td>
     <td class="text-center">
         <button type="button" class="deleteBtn btn btn-danger">Delete</button>
     </td>
-  </tr>
-  `;
+  </tr>`;
+    })
+    .join('');
+
+  // users.forEach((user) => {
+  //   return (tbodyEl.innerHTML += `
+  // <tr>
+  //   <th scope="row">${user.id}</th>
+  //   <td>${user.firstname}</td>
+  //   <td>${user.lastname}</td>
+  //   <td>${user.address}</td>
+  //   <td>${user.gender}</td>
+  //   <td>${user.dateBirth}</td>
+  //   <td class="text-center">
+  //       <button type="button" class="deleteBtn btn btn-danger">Delete</button>
+  //   </td>
+  // </tr>
+  // `);
+  // });
+
+  clearForm();
+};
+
+const clearForm = () => {
+  firstname.value = '';
+  lastname.value = '';
+  address.value = '';
+  gender.value = 'male';
+  dateBirth.value = '';
+  note.value = '';
+  formEl.classList.remove('was-validated');
 };
 
 const onDeleteRow = (event) => {
@@ -42,9 +100,12 @@ const onDeleteRow = (event) => {
   }
 
   const btn = event.target;
+  const index = btn.parentNode.parentNode.rowIndex;
+
+  const filtered = users.filter((user) => user.id !== index);
+  users = filtered;
   btn.closest('tr').remove();
 };
 
-formEl.addEventListener('submit', validation);
 formEl.addEventListener('submit', onAddWebsite);
 tableEl.addEventListener('click', onDeleteRow);
